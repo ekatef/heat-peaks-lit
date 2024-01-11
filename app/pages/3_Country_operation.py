b8aee964558addc76f1840d53a95eb9530793c5a
ekatef <e.v.kasilova@gmail.com>
2024-01-11 14:30:01 +0300

    Move processing of bus dataframes into a dedicated tab

--- app/pages/3_Country_operation.py
+++ app/pages/3_Country_operation.py
@@ -42,11 +42,9 @@ non_empth_links_keys = [param for param in helper.config["links_t_parameter"]]
 non_empth_loads_keys = [param for param in helper.config["loads_t_parameter"]]
 non_empth_stores_keys = [param for param in helper.config["stores_t_parameter"]]
 
-# average by carrier
-gen_dict_list = helper.get_gen_t_dict()
-#storage_df = helper.get_storage_t_dict()
-loads_dict_list = helper.get_load_t_dict()
-#stores_df = helper.get_components_t_dict("stores_t", non_empth_stores_keys)
+# carrier values per bus
+gen_buses_dict_list = helper.get_buses_gen_t_dict()
+load_buses_dict_list = helper.get_buses_load_t_dict()
 
 res_choices = helper.config["operation"]["resolution"]
 
@@ -89,7 +87,7 @@ tools.add_logo()
 with main_col:
     selected_network = st.selectbox(
         "Select which scenario's plot you want to see :",
-        list(gen_dict_list.keys()),
+        list(gen_buses_dict_list.keys()),
         format_func = scenario_formatter,
         help="You can choose between available scenarios"
     )
@@ -115,15 +113,22 @@ with suppl_col:
     )
     st.markdown(fix_cursor_css, unsafe_allow_html=True) 
 
-country_data = gen_dict_list.get(selected_network)
+# # TODO naming should be imroved
+# country_data = gen_dict_list.get(selected_network)
+buses_country_data = gen_buses_dict_list.get(selected_network)
+buses_load_country_data = load_buses_dict_list.get(selected_network)
 
 ##################### generators #####################
-gen_df = country_data["p"].drop("Load", axis=1, errors="ignore")
+# gen_df = country_data["p"].drop("Load", axis=1, errors="ignore")
+# TODO naming should be imroved: gen_buses_df can be distinguished 
+# between the dataframe and the dictionary
+gen_buses_df = buses_country_data["p"].drop("Load", axis=1, errors="ignore")
+load_buses_df = buses_load_country_data["p"]
 
 _, date_range_param, _ = st.columns([1, 50, 1])
 with date_range_param:
-    min_index = gen_df.index[0]
-    max_index = gen_df.index[-1]
+    min_index = gen_buses_df.index[0]
+    max_index = gen_buses_df.index[-1]
     min_value = datetime.datetime(min_index.year, min_index.month, min_index.day)
     max_value = datetime.datetime(max_index.year, max_index.month, max_index.day)
     values = st.slider(
@@ -135,47 +140,39 @@ with date_range_param:
             key="gen_date"
         )
 
-###################### demand #####################
-loads_country_data=loads_dict_list.get(selected_network)
-#stores_country_data=stores_df.get(selected_network)
+# ###################### generation #####################
 
-loads_df = loads_country_data["p"]
-heat_loads_df = loads_df.filter(like="Heating")
+# # TODO wouldn't it be more reliabe to move columns renaming there?
+# #gen_df.columns = [tech_map[c] for c in gen_df.columns]
 
-###################### generation #####################
-
-# TODO wouldn't it be more reliabe to move columns renaming there?
-#gen_df.columns = [tech_map[c] for c in gen_df.columns]
-
-#TODO Add balance?
-balance_df = gen_df
+# #TODO Add balance?
+# balance_df = gen_df
 
 # TODO Check if res contains only numbers
 res_h = str(res) + "H"
 
-balance_aggr = balance_df.loc[values[0]:values[1]].resample(res_h).mean()
-heat_aggr = heat_loads_df.loc[values[0]:values[1]].resample(res_h).mean()
-
-heat_aggr["Overall"] = heat_aggr.sum(axis=1)
-# TODO make filtering case insensitive/based on regex
-heat_aggr["Overall Flatten"] = heat_aggr["Overall"] - balance_aggr.filter(like="etrofi").filter(like="oder").sum(axis=1)
-
 _, balance_plot_col, _ = st.columns([1, 80, 1])
 
+gen_buses_aggr = gen_buses_df.loc[values[0]:values[1]].resample(res_h).mean()
+load_buses_aggr = load_buses_df.loc[values[0]:values[1]].resample(res_h).mean()
+
+# TODO Add a selector box
+country_code = "PL"
+
+gen_buses_aggr = gen_buses_aggr.filter(like=country_code)
+load_buses_aggr = load_buses_aggr.filter(like=country_code)
+
 with balance_plot_col:
-     heat_dem_area_plot=heat_aggr.filter(like="eat").hvplot.area(
-         **kwargs,
-         ylabel="Heat Demand [MW]",
-         group_label=helper.config["loads_t_parameter"]["p"]["legend_title"],
-         color = ["#ffc100", "#ff9a00", "#ff7400", "#ff4d00", "#ff0000"]
-         )
-     heat_dem_line_plot=heat_aggr[["Overall", "Overall Flatten"]].hvplot.line(
-         color = ["#333333", "#777777",]
-     )
-     heat_dem_area_plot = heat_dem_area_plot.opts(
-         fontsize=plot_font_dict
-     )         
-     s2=hv.render(heat_dem_area_plot*heat_dem_line_plot, backend="bokeh")
-     st.bokeh_chart(s2, use_container_width=True)
+    buses_gen_area_plot = load_buses_aggr.filter(like="Heating").hvplot.area(
+        **kwargs,
+        ylabel="Heat Demand [MW]",
+        group_label=helper.config["loads_t_parameter"]["p"]["legend_title"],
+        color = ["#ffc100", "#ff9a00", "#ff7400", "#ff4d00", "#ff0000"]
+        )
+    buses_gen_area_plot = buses_gen_area_plot.opts(
+        fontsize=plot_font_dict
+    )         
+    s2=hv.render(buses_gen_area_plot, backend="bokeh")
+    st.bokeh_chart(s2, use_container_width=True)    
 
 tools.add_logo()  
